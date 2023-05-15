925052987aa1a44be26d00c9247fac0e62a23102
Jingjing Wang <rileywang0819@gmail.com>
2023-05-15 20:43:09 +0900

    done with lab01: functions, control

---
+++ lab/lab01/lab01.py
@@ -0,0 +1,70 @@
+# Q5: Falling Factorial
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    result = 1
+    while k > 0:
+        result = result * n
+        n, k = n - 1, k - 1
+    return result
+
+
+# Q6: Sum Digits
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    digits_sum = 0
+    while y > 0:
+        y, last_digit = y // 10, y % 10
+        digits_sum += last_digit
+    return digits_sum
+
+
+# Q8: Double Eights
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    prev_eight = False
+    while n > 0:
+        n, last_digit = n // 10, n % 10
+        if prev_eight and last_digit == 8:
+            return True
+        elif last_digit == 8:
+            prev_eight = True
+        else:
+            prev_eight = False
+    return False
